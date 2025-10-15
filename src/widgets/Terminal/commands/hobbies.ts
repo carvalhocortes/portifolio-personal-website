489380736ba4a94d9135752596dac123f0e288cb
CarvalhoCortes <carvalhocortes@gmail.com>
2025-10-14 21:42:20 -0300

    feat: update hobbies section with detailed descriptions and images

--- src/widgets/Terminal/commands/hobbies.ts
+++ src/widgets/Terminal/commands/hobbies.ts
@@ -1,20 +1,57 @@
-import { formatObjectArray, type TextObject } from "./render-text";
+
 
 export const hobbies = (): string => {
-  return formatObjectArray(hobbiesPtBr);
+  return hobbiesPtBr;
 };
 
-const hobbiesPtBr: TextObject = {
-  Marcenaria: [
-    'Meu avô materno era uma pessoa super habilidosa. Me recordo dele consertando gavetas de armários e fazendo brinquedos para a gente, como carrinhos ou casinhas de boneca. Lembro do cheiro da madeira e do seu armário de ferramentas como se tivesse sido ontem a última vez que o vi. Outra pessoa que me inspirou a trabalhar com madeira foi o amigo do meu pai apelidado de Taboca, que é marceneiro. Juntos, ele e meu pai inventaram alguns dos brinquedos mais legais que tive.',
-    'No meu apartamento eu tenho uma pequena oficina que me permitiu criar algumas coisas. Por exemplo um banco para a entrada de casa, onde nos sentamos para (des)calçar os sapatos. O que mais gosto nesse projeto é que utilizei a madeira que recuperei dos batentes das porta quando reformei o apartamento. Uma segunda peça que fiz também fica na entrada do apartamento e foi pensada para acomodar as coisas que levamos para a rua, como chaves e bolsas. E por fim uma escrivaninha. Ela tem mais de 2 metros de comprimento e é usada dos dois lados, por mim e pela minha esposa, de forma que podemos nos ver enquanto trabalhamos no computador.'
-  ],
-  Fotografia: [
-    'Só se vive uma vez, certo? Eu adoro viajar e conhecer coisas novas, pessoas novas, lugares novos. Mas me parece que as lembranças vão indo embora um pouco de cada vez, né?',
-    'Eu comecei a fotografar incentivado por um amigo do trabalho, para me inscrever em concursos fotográficos. Daí virou paixão e hoje fotografo para me lembrar dos lugares lindos que já visitei.',
-  ],
-  'DIY e Impressão 3D': [
-    'Várias pessoas, em diversos momentos, já me questionaram sobre o motivo de eu gostar tanto da engenharia. E a resposta vai ser sempre a mesma: porque me ensinou que posso fazer qualquer coisa.',
-    'Minha cabeça vive cheia de ideias. Estou sempre inventando alguma coisa, seja para a casa ou só por diversão. Sou apaixonado por criar coisas. Eu já fiz mesa de jantar, várias facas, uma luminária que se liga pela internet e esculturas de plástico, impressas e de sucata. Também me divirto programando bots aleatórios ou filtros para minhas fotos e vídeos. Coisa de maluco mesmo!'
-  ]
-}
+
+const marcenariaInfo1 = 'Banco do hall do apartamento';
+const marcenariaInfo2 = 'Porta chaves e bolsas no hall do apartamento';
+const marcenariaInfo3 = 'Escrivaninha para duas pessoas';
+const marcenariaImg1 = `![${marcenariaInfo1}](./src/resources/banco.png)`;
+const marcenariaImg2 = `![${marcenariaInfo2}](./src/resources/guarda-bolsa.png)`;
+const marcenariaImg3 = `![${marcenariaInfo3}](./src/resources/mesa-madeira.png)`;
+
+const fotografiaInfo1 = 'Arara Azul - Nobres - MT';
+const fotografiaInfo2 = 'Casal de Araras Vermelhas - Belem - PA';
+const fotografiaInfo3 = 'Pássaro Preto - São Paulo - SP';
+const fotografiaInfo4 = 'Lagoa - Rio de Janeiro - RJ';
+const fotografiaImg1 = `![${fotografiaInfo1}](./src/resources/arara-azul.png)`;
+const fotografiaImg2 = `![${fotografiaInfo2}](./src/resources/araras-vermelhas.png)`;
+const fotografiaImg3 = `![${fotografiaInfo3}](./src/resources/passaro-preto.png)`;
+const fotografiaImg4 = `![${fotografiaInfo4}](./src/resources/rio-lagoa.png)`;
+
+const diyInfo1 = 'Cranio em estilo voronoy. Impresso em PLA';
+const diyInfo2 = 'Mesa de jantar com oito lugares';
+const diyInfo3 = 'Luminária inteligente';
+const diyImg1 = `![${diyInfo1}](./src/resources/voronai.png)`;
+const diyImg2 = `![${diyInfo2}](./src/resources/mesa.png)`;
+const diyImg3 = `![${diyInfo3}](./src/resources/luminaria.png)`;
+
+
+const hobbiesPtBr = `### Marcenaria
+  Meu avô materno era uma pessoa super habilidosa. Me recordo dele consertando gavetas de armários e fazendo brinquedos para a gente, como carrinhos ou casinhas de boneca. Lembro do cheiro da madeira e do seu armário de ferramentas como se tivesse sido ontem a última vez que o vi.
+  Outra pessoa que me inspirou a trabalhar com madeira foi o amigo do meu pai apelidado de Taboca, que é marceneiro.Juntos, ele e meu pai inventaram alguns dos brinquedos mais legais que tive.
+  Nas fotos abaixo mostro um pouquinho das peças que já fiz. Um banco para a entrada de casa, onde nos sentamos para (des)calçar os sapatos. O que mais gosto nesse projeto é que utilizei a madeira que recuperei dos batentes das porta quando reformei o apartamento. A segunda peça também fica na entrada do apartamento e foi pensada para acomodar as coisas que levamos para a rua, como chaves e bolsas. E a última é uma escrivaninha. Ela tem mais de 2 metros de comprimento e é usada dos dois lados, por mim e pela minha esposa, de forma que podemos nos ver enquanto trabalhamos no computador.
+
+  | ${marcenariaInfo1} | ${marcenariaInfo2} | ${marcenariaInfo3} |
+  | --- | --- | --- |
+  | ${marcenariaImg1} | ${marcenariaImg2} | ${marcenariaImg3} |
+
+  ### Fotografia
+  Só se vive uma vez, certo? Eu adoro viajar e conhecer coisas novas, pessoas novas, lugares novos. Mas me parece que as lembranças vão indo embora um pouco de cada vez, né?
+  Eu comecei a fotografar incentivado por um amigo do trabalho, para me inscrever em concursos fotográficos. Daí virou paixão e hoje fotografo para me lembrar dos lugares lindos que já visitei.
+
+  | ${fotografiaInfo1} | ${fotografiaInfo2} | ${fotografiaInfo3} | ${fotografiaInfo4} |
+  | --- | --- | --- | --- |
+  | ${fotografiaImg1} | ${fotografiaImg2} | ${fotografiaImg3} | ${fotografiaImg4} |
+
+  (Curiosidade: você sabia que as araras formam casais para se reproduzir e depois nunca mais se separam? Quando uma morre a outra vive solitária até o fim da sua vida. Fonte) [Aqui a Fonte](https://g1.globo.com/sp/itapetininga-regiao/noticia/2019/06/12/biologa-do-interior-de-sp-explica-sobre-relacionamento-entre-araras-sem-traicao.ghtml))
+
+  ### DIY e Impressão 3D
+  Várias pessoas, em diversos momentos, já me questionaram sobre o motivo de eu gostar tanto da engenharia. E a resposta vai ser sempre a mesma: porque me ensinou que posso fazer qualquer coisa.
+  Minha cabeça vive cheia de ideias. Estou sempre inventando alguma coisa, seja para a casa ou só por diversão. Sou apaixonado por criar coisas. Eu já fiz mesa de jantar, várias facas, uma luminária que se liga pela internet e esculturas de plástico, impressas e de sucata. Também me divirto programando bots aleatórios ou filtros para minhas fotos e vídeos. Coisa de maluco mesmo!
+
+  | ${diyInfo1} | ${diyInfo2} | ${diyInfo3} |
+  | --- | --- | --- |
+  | ${diyImg1} | ${diyImg2} | ${diyImg3} |`
