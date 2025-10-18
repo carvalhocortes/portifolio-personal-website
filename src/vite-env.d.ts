82482ed56ca9dd901da4e6e9d58f0daaa0d20100
CarvalhoCortes <carvalhocortes@gmail.com>
2025-10-18 14:45:17 -0300

    feat: add CSS Modules type definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,11 @@
 /// <reference types="vite/client" />
 
-declare module "*.css" {
+declare module '*.css' {
   const content: Record<string, string>;
   export default content;
+}
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
 }
