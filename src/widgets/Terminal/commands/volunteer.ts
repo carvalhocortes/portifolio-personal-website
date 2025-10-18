64ba42b4a2438c60425bdd93020d1471658a421b
CarvalhoCortes <carvalhocortes@gmail.com>
2025-10-18 11:53:34 -0300

    feat: melhar aspecto pagina voluntarios

--- src/widgets/Terminal/commands/volunteer.ts
+++ src/widgets/Terminal/commands/volunteer.ts
@@ -1,21 +1,46 @@
-import { formatObjectArray, type TextObject } from "./render-text";
-
 export const volunteer = (): string => {
-  return formatObjectArray(volunteerPtBr);
+  return volunteerPtBr;
 };
 
-const volunteerPtBr: TextObject = {
-  'A Ordem DeMolay': [
-    'A Ordem DeMolay é um grupo de jovens patrocinado e apoiado pela maçonaria desde 1919. Foi criado nos Estados Unidos da América por um Maçom da cidade de Kansas City.',
-    'Tem por objetivo criar bons cidadãos, que respeitam as leis, que convivem em harmonia com a sociedade, que auxiliam o próximo em suas necessidades básicas e educacionais e que, por meio do exemplo, sirvam como modelo a ser seguido por todos os jovens.',
-    'Faço parte da Ordem desde os 13 anos. Lá organizei e participei de campanhas de doação de alimentos e roupas, jantares beneficentes e distribuição de cestas básicas.'
-  ],
-  'Fundação Telefônica VIVO': [
-    'Com foco em educação como pilar essencial de transformação da sociedade, a Fundação Telefônica VIVO contribui com o desenvolvimento e inclusão digital de estudantes e educadores por meio de projetos que estimulam novas oportunidades de ensino e aprendizagem.',
-    'O Dia dos Voluntários é um projeto global que envolve os colaboradores de todas as empresas do Grupo Telefônica. Neste dia, um projeto é escolhido e tem sua sede reformada pelos voluntários. Durante a reforma os ocupantes da sede se juntam a um segundo grupo para atividades de lazer.',
-  ],
-  'Minha Cena': [
-    'O MinhaCena.Org é uma ferramenta de impacto social e educacional que propõe estímulo para melhora na escrita e leitura com a produção de ilustrações para redações de alunos de escolas públicas, privadas e ONGs de contraturno escolar com apoio de voluntários.',
-    'Neste projeto estamos criando uma plataforma on-line e eu contribuo participando do time de back-end com outros voluntários.',
-  ],
-}
+const vText1 = 'A Ordem DeMolay é um grupo de jovens patrocinado e apoiado pela maçonaria desde 1919. Foi criado nos Estados Unidos da América por um Maçom da cidade de Kansas City. Tem por objetivo criar bons cidadãos, que respeitam as leis, que convivem em harmonia com a sociedade, que auxiliam o próximo em suas necessidades básicas e educacionais e que, por meio do exemplo, sirvam como modelo a ser seguido por todos os jovens. Faço parte da Ordem desde os 13 anos. Lá organizei e participei de campanhas de doação de alimentos e roupas, jantares beneficentes e distribuição de cestas básicas.'
+
+const vLabel1 = 'Logo da Ordem DeMolay'
+const vImg1 = `![${vLabel1} ](./resources/demolay.png)`
+
+const vText2 = 'Com foco em educação como pilar essencial de transformação da sociedade, a Fundação Telefônica VIVO contribui com o desenvolvimento e inclusão digital de estudantes e educadores por meio de projetos que estimulam novas oportunidades de ensino e aprendizagem. O Dia dos Voluntários é um projeto global que envolve os colaboradores de todas as empresas do Grupo Telefônica.Neste dia, um projeto é escolhido e tem sua sede reformada pelos voluntários.Durante a reforma os ocupantes da sede se juntão a um segundo grupo para atividades lazer. Participação no dia dos voluntários de 2008 até 2010.'
+
+const vLabel2_1 = 'Logo Fundação Telefônica Vivo'
+const vImg2_1 = `![${vLabel2_1} ](./resources/fundacao-telefonica-vivo.png)`
+
+const vLabel2_2 = 'Capa da revista do ano de 2009. /n Na foto apareço do lado de uma criança que participou do projeto.'
+const vImg2_2 = `![${vLabel2_2} ](./resources/voluntarios-tele.jpg)`
+
+const vText3 = 'O MinhaCena.Org é uma ferramenta de impacto social e educacional que propõe estímulo para melhora na escrita e leitura com a produção de ilustrações para redações de alunos de escolas públicas, privadas e ONGs de contraturno escolar com apoio de voluntários. Neste projeto estamos criando uma plataforma on-line e eu contribuo participando do time de back-end com outros voluntários.'
+
+const vLabel3 = 'Logo Minha Cena Org'
+const vImg3 = `![${vLabel3} ](./resources/minha-cena.jpeg)`
+
+const volunteerPtBr = `## Trabalho Voluntário
+
+### [Ordem DeMolay](https://www.demolay.org.br/)
+
+|     |   |
+| --- | --- |
+| ${vImg1} | ${vText1} |
+
+
+### [Fundação Telefonica VIVO](https://www.fundacaotelefonicavivo.org.br/)
+
+|     |   |   |
+| --- | --- | --- |
+| ${vImg2_1} | ${vText2} |
+| ${vImg2_2} | |
+| ${vLabel2_2} |  |
+
+## [Minha Cena](https://www.instagram.com/projetominhacena/)
+
+|     |   |
+| --- | --- |
+| ${vImg3} | ${vText3} |
+`
+
